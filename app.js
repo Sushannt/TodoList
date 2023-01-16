07db4673e87ca131559900fb0c5536504e18126c
iSushaant <kumar.su5hant@yahoo.com>
2023-01-16 16:21:00 +0530

    created a mongodb database for storing tasks

--- app.js
+++ app.js
@@ -2,13 +2,45 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
 const date = require(__dirname + "/date.js");  
+const dbName = 'todolistDB';
+const url = `mongodb://localhost:27017/${dbName}`;
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+
+//connecting to database
+mongoose.connect(url);
+
+const itemsSchema = new mongoose.Schema({
+    name : String
+}) 
+
+const Item = new mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!!"
+});
+const item2 = new Item({
+  name: "hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, (err)=>{
+  (err)
+  ? console.log(err)
+  : console.log("document created successfully")
+});
+
 
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
